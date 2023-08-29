69f26a6845481a0b6f80312797ce1aa5f72c69a3
EvanSarwer <evansarwer1@gmail.com>
2023-08-30 00:38:21 +0600

    little error update

--- resources/views/main/seasonal_holidays.blade.php
+++ resources/views/main/seasonal_holidays.blade.php
@@ -55,10 +55,10 @@
     <!-- Team Start -->
     <div class="container-xxl py-5">
         <div class="container">
-            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <!-- <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
                 <h6 class="text-primary text-uppercase">// Our Technicians //</h6>
                 <h1 class="mb-5">Our Expert Technicians</h1>
-            </div>
+            </div> -->
             <div class="row">
 
 
